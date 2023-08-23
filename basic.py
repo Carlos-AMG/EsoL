b243002745134a2872725750b6c7ef8c8c81a681
Carlos-AMG <alberto-cmg@outlook.es>
2023-08-23 00:01:13 -0600

    Parser done, working on parsing errors and some other functionalities

--- basic.py
+++ basic.py
@@ -1,17 +1,6 @@
+from utils import string_with_arrows
 
-
-DIGITS = "0123456789"
-
-TT_INT = "TT_INT"
-TT_FLOAT = "FLOAT"
-TT_PLUS = "PLUS"
-TT_MINUS = "MINUS"
-TT_MUL = "MUL"
-TT_DIV = "DIV"
-TT_LPAREN = "LPAREN"
-TT_RPAREN = "RPAREN"
-
-
+# errors
 class Error:
     def __init__(self, pos_start, pos_end, error_name, details) -> None:
         self.pos_start = pos_start
@@ -22,12 +11,20 @@ class Error:
     def as_string(self):
         result = f'{self.error_name}: {self.details}\n'
         result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
         return result
     
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details) -> None:
         super().__init__(pos_start, pos_end, "Illegal character", details=details) 
 
+class InvalidSyntaxError(Error):
+    def __init__(self, pos_start, pos_end, details="") -> None:
+        super().__init__(pos_start, pos_end, "Invalid Syntax" , details)
+
+# idx -> index
+# ln -> line number
+# col -> column number
 class Position:
     def __init__(self, idx, ln, col, fn, ftxt):
         self.idx = idx
@@ -46,16 +43,36 @@ class Position:
     
     def copy(self):
         return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+    
+
+# tokens and class token
+DIGITS = "0123456789"
+
+TT_INT = "TT_INT"
+TT_FLOAT = "FLOAT"
+TT_PLUS = "PLUS"
+TT_MINUS = "MINUS"
+TT_MUL = "MUL"
+TT_DIV = "DIV"
+TT_LPAREN = "LPAREN"
+TT_RPAREN = "RPAREN"
 
 class Token:
-    def __init__(self, type, value=None) -> None:
+    def __init__(self, type, value=None, pos_start=None, pos_end=None) -> None:
         self.type = type
         self.value = value
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
+        if pos_end:
+            self.pos_end = pos_end.copy()
     def __repr__(self):
         if self.value: 
             return f'{self.type}:{self.value}'
         return f'{self.type}'
     
+# Lexer section
 class Lexer:
     def __init__(self, fn, text) -> None:
         self.fn = fn
@@ -117,7 +134,83 @@ class Lexer:
         else:
             return Token(TT_FLOAT, float(num_str))
         
+# nodes
+
+class NumberNode:
+    def __init__(self, token) -> None:
+        self.token = token
+    def __repr__(self) -> str:
+        return f'{self.token}'
+
+# op_token -> operator_token
+class BinOpNode:
+    def __init__(self, left_node, right_node, op_token) -> None:
+        self.left_node = left_node
+        self.right_node = right_node
+        self.op_token = op_token
+
+    def __repr__(self) -> str:
+        return f'({self.left_node}, {self.op_token}, {self.right_node})'
+
+# Parse result
+
+# class ParseResult:
+#     def __init__
+
+
+# Parser class
+class Parser:
+    def __init__(self, tokens) -> None:
+        self.tokens = tokens
+        self.tok_idx = -1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_token = self.tokens[self.tok_idx]
+        
+        return self.current_token
+    
+    # methods
+    def parse(self):
+        res = self.expr()
+        return res
+    
+    def factor(self):
+        token = self.current_token
+        if token.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(token)
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+    
+    def bin_op(self, func, ops):
+        """Because both expr and term have the same grammar, they share the same function only differing on what the BinOpNode will contain
+        So we modularize this into a single function that handles both cases"""
+        left = func()
+        while self.current_token.type in ops:
+            op_token = self.current_token
+            self.advance()
+            right = func()
+            left = BinOpNode(left, right, op_token)
+        return left
+        
+# run segment        
 def run(fn, text):
+    # Generate tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
-    return tokens, error
+    if error:
+        return None, error
+    
+    # print("Tokens: ", tokens)
+
+    # Generate AST (Abstract Syntax Tree)
+    parser = Parser(tokens)
+    ast = parser.parse()
+    return ast, None
